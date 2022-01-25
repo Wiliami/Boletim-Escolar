b6b8bee977a171e87f214dd71e43d6e9e93a39d8
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-25 19:04:01 -0400

    adicionando arquivo que contém uma lista de times

---
+++ teams.js
@@ -0,0 +1,18 @@
+const listaTimes = {
+    Flamengo: {
+        result: '<a class="teams" href="https://www.google.com/search?q=flamengo&oq=flamengo&aqs=chrome.0.69i59j0i433l3j0i131i433j0j69i60l2.1547j1j7&sourceid=chrome&ie=UTF-8"> <p>Acompanhe aqui os jogos do Flamengo!</p></a>',
+        name: 'Flamengo',
+    },
+    Vasco: {
+        result: '<p class="teams">Desculpa, mas você não tem um bom gosto!</p>',
+        name: 'Vasco',
+    },
+    Palmeiras: {
+        result: '<a class="teams" href="https://br.pinterest.com/pin/727683252291710112/"><p>Palmeiras não tem mundial!</p></a>',
+        name: 'Palmeiras',
+    },
+    Corinthians: {
+        result: '<a class="teams" href="https://www.google.com/search?q=corinthinas&oq=corinthinas+&aqs=chrome..69i57j0i10l2j46i10j0i10l6.5599j0j7&sourceid=chrome&ie=UTF-8"><p>Clique aqui!</p></a>',
+        name: 'Corinthians',
+    }
+}
